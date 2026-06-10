b00cb0ea72d26d197615ec18944d34a72091272f
tem2546 <temtem.sao@gmail.com>
2026-06-10 20:56:22 +0900

    fix gitignore & combustion_simulator.m

--- Combustion_Simulator.m
+++ Combustion_Simulator.m
@@ -14,22 +14,25 @@ list.oxidant={'N2O'}; list.fuel={'PE','PP','ABS'};
 cd('Scripts');
 
 %モードの選択
-mode = listdlg('PromptString','モードを選択',...
+[mode, tf] = listdlg('PromptString','モードを選択',...
     'Name','Select mode','SelectionMode','Single',...
     'ListString',list.mode);
 disp(strcat(list.mode(mode),'モードを実行します。'))
 
-switch mode
-    case 1 %データ整理用
-        Class = Mode1_Organize_data().run();
-    case 2 %データ比較用
-        Class = Mode2_Compare_data().run();
-    case 3 %CEA_GUI操作用
-        Class = Mode3_CEA_GUI().run();
-    case 4 %自作エンジン解析用
-        Class = Mode4_Analyze_HomebrewEngine().run(list);
-    case 5 %自作エンジン設計用
-        Class = Mode5_Design_HomebrewEngine().run(list);        
+if tf
+    switch mode
+        case 1 %データ整理用
+            Class = Mode1_Organize_data().run();
+        case 2 %データ比較用
+            Class = Mode2_Compare_data().run();
+        case 3 %CEA_GUI操作用
+            Class = Mode3_CEA_GUI().run();
+        case 4 %自作エンジン解析用
+            Class = Mode4_Analyze_HomebrewEngine().run(list);
+        case 5 %自作エンジン設計用
+            Class = Mode5_Design_HomebrewEngine().run(list);        
+    end
 end
+
 cd('../');
 disp('終了');
